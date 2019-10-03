ea36f8d0adc24428e553fcf98cb6cfe20179b57e
Ozarrian2026 <56038662+Ozarrian2026@users.noreply.github.com>
2019-10-03 08:49:40 -0700

    Add files via upload

--- dice.py
+++ dice.py
@@ -1,3 +1,14 @@
-# Name:
-# Period
+# Ozarrian John
+# Block 1
 # Dice Rolling Simulator
+
+import random
+
+numRolls = int(input("Enter the amount of rolls: "))
+x = 1
+
+while x <= numRolls:
+	print("Total Rolls: " + str(numRolls))
+	break
+
+	x += 1
