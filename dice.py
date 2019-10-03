8a12b3acd43a8a6c58d8ae8704671c3e82c0610f
Ozarrian2026 <56038662+Ozarrian2026@users.noreply.github.com>
2019-10-03 09:03:27 -0700

    Add files via upload

--- dice.py
+++ dice.py
@@ -6,9 +6,14 @@ import random
 
 numRolls = int(input("Enter the amount of rolls: "))
 x = 1
+1s = 0
+2s = 0
+3s = 0
+4s = 0
+5s = 0
+6s = 0
 
 while x <= numRolls:
 	print("Total Rolls: " + str(numRolls))
 	break
-
-	x += 1
+	
