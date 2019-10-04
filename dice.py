b83673092771a890f9f990c8a5a7abecc0142309
Ozarrian2026 <56038662+Ozarrian2026@users.noreply.github.com>
2019-10-04 08:43:10 -0700

    Add files via upload

--- dice.py
+++ dice.py
@@ -18,14 +18,26 @@ while rollNum <= numRolls:
 	randomNum = random.randint(1, 6) 
 	if randomNum == 1:
 		print("Roll number: " + str(randomNum))
+		one = one + 1
 	if randomNum == 2:
 		print("Roll number: " + str(randomNum))
+		two = two + 1
 	if randomNum == 3:
 		print("Roll number: " + str(randomNum))
+		three = three + 1
 	if randomNum == 4:
 		print("Roll number: " + str(randomNum))
+		four = four + 1
 	if randomNum == 5:
 		print("Roll number: " + str(randomNum))
+		five = five + 1
 	if randomNum == 6:
 		print("Roll number: " + str(randomNum))
+		six = six + 1
 	break
+print("1s: " + str(one))
+print("2s: " + str(two))
+print("3s: " + str(three))
+print("4s: " + str(four))
+print("5s: " + str(five))
+print("6s: " + str(six))
