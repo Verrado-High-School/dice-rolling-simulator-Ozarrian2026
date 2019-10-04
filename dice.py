275446c3f31a00f0e446f91d3c6b94e805214320
Ozarrian2026 <56038662+Ozarrian2026@users.noreply.github.com>
2019-10-04 08:33:06 -0700

    Add files via upload

--- dice.py
+++ dice.py
@@ -5,15 +5,27 @@
 import random
 
 numRolls = int(input("Enter the amount of rolls: "))
-x = 1
-1s = 0
-2s = 0
-3s = 0
-4s = 0
-5s = 0
-6s = 0
 
-while x <= numRolls:
+one = 0
+two = 0
+three = 0
+four = 0
+five = 0
+six = 0
+rollNum = 1
+while rollNum <= numRolls:
 	print("Total Rolls: " + str(numRolls))
+	randomNum = random.randint(1, 6) 
+	if randomNum == 1:
+		print("Roll number: " + str(randomNum))
+	if randomNum == 2:
+		print("Roll number: " + str(randomNum))
+	if randomNum == 3:
+		print("Roll number: " + str(randomNum))
+	if randomNum == 4:
+		print("Roll number: " + str(randomNum))
+	if randomNum == 5:
+		print("Roll number: " + str(randomNum))
+	if randomNum == 6:
+		print("Roll number: " + str(randomNum))
 	break
-	
